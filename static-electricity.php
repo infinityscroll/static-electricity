d58c12df4e8d29eeaffdca1aee114da4d6c2a719
Paul Houser <paul@barn2.com>
2022-01-23 15:39:26 -0500

    scaffolding generated

---
+++ static-electricity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Static Electricity
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     static-electricity
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Static_Electricity
+ */
+
+// Your code starts here.
